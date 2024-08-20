5105249076ac9fae89a4d0e71f39f34892a8923e
SHOPBX1 <159017705+SHOPBX1@users.noreply.github.com>
2024-08-20 23:58:02 +0200

    Update index.tsx

--- dinholion-storefront/src/modules/layout/components/side-menu/index.tsx
+++ dinholion-storefront/src/modules/layout/components/side-menu/index.tsx
@@ -85,7 +85,7 @@ const SideMenu = ({ regions }: { regions: Region[] | null }) => {
                         />
                       </div>
                       <Text className="flex justify-between txt-compact-small">
-                        © {new Date().getFullYear()} Medusa Store. All rights
+                        © {new Date().getFullYear()} DINHO LION VTC. All rights
                         reserved.
                       </Text>
                     </div>
