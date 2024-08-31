a4f1900a01ab77af4693fc953a7ed522db8a979a
SHOPBX1 <159017705+SHOPBX1@users.noreply.github.com>
2024-08-31 10:45:01 +0200

    Update index.tsx

--- dinholion-storefront/src/modules/layout/components/medusa-cta/index.tsx
+++ dinholion-storefront/src/modules/layout/components/medusa-cta/index.tsx
@@ -6,13 +6,25 @@ import NextJs from "../../../common/icons/nextjs"
 const MedusaCTA = () => {
   return (
     <Text className="flex gap-x-2 txt-compact-small-plus items-center">
-      Powered by
+      Services
       <a href="https://www.medusajs.com" target="_blank" rel="noreferrer">
-        <Medusa fill="#9ca3af" className="fill-[#9ca3af]" />
+         
       </a>
-      &
+      A propos
+      <a href="https://www.medusajs.com" target="_blank" rel="noreferrer">
+        
+      </a>
+      FAQs
+      <a href="https://www.medusajs.com" target="_blank" rel="noreferrer">
+       
+      </a>
+      CGV & Mentions Légales
       <a href="https://nextjs.org" target="_blank" rel="noreferrer">
-        <NextJs fill="#9ca3af" />
+        
+      </a>
+      Confidentialité
+      <a href="https://nextjs.org" target="_blank" rel="noreferrer">
+         
       </a>
     </Text>
   )
